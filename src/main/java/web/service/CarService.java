085c18a7449638bf4f4916f484b9e531d47b4351
Tomikus <tomikus1@gmail.com>
2023-11-29 15:26:43 +0300

    cars init and getCars logic moved to Service, CarService init changed to DI @Autowired way

--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -5,5 +5,5 @@ import web.model.Car;
 import java.util.List;
 
 public interface CarService {
-    List<Car> getCars(int count, List<Car> cars);
+    List<Car> getCars(int count);
 }
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,12 +1,22 @@
 package web.service;
 
+import org.springframework.stereotype.Component;
 import web.model.Car;
 
+import java.util.Arrays;
 import java.util.List;
 
+@Component
 public class CarServiceImpl implements CarService {
+    List<Car> cars = Arrays.asList(
+            new Car("BMW", "red", 150),
+            new Car("VW", "blk", 100),
+            new Car("NIS", "sil", 110),
+            new Car("KIA", "gre", 120),
+            new Car("AU", "wht", 170));
+
     @Override
-    public List<Car> getCars(int count, List<Car> cars) {
-        return cars.subList(0, count);
+    public List<Car> getCars(int count) {
+        return cars.subList(0, Math.min(count, 5));
     }
 }
